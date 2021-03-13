d25cfb0ae53ea300e5e9ea87800e08f1cbe18880
JAN SEBASTIAN RODRIGUEZ PENA <rodriguezjanp@HILDEBOG.LOCAL>
2021-03-13 13:15:46 -0500

    ssss commit

---
+++ JavaScript/Practica.js
@@ -0,0 +1,9 @@
+function holamundo(){
+    var numero = 1;
+    var numerodos = 3;
+    var input = document.getElementById("Num1")
+    var input2 = document.getElementById("Num2")
+
+    alert(parseInt(input.value) + parseInt(input2.value));
+}
+
